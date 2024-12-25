fea307350a1b65593bb2cff9e077dd1f0009e6a3
EladWebiks <elad_harel@webiks.com>
2024-12-25 09:55:33 +0200

    added call for render

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,20 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [message,setMessage]  = useState("HELLO")
 
+  useEffect(() => {
+    axios.get(import.meta.env.VITE_BASE_URL).then(res=>setMessage(res.data))
+  }, [])
+  
   return (
     <>
+    <h1>{message}</h1>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
